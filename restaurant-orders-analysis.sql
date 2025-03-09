9420fe3f329719719b075c224f04737c371f5bf1
Kristóf Németh <138215045+SnickleSH@users.noreply.github.com>
2025-03-09 16:03:07 +0100

    All tasks completed

--- restaurant-orders-analysis.sql
+++ restaurant-orders-analysis.sql
@@ -117,11 +117,97 @@ FROM (
 -- Objective 3: Analyze customer behavior
 
 -- Task: Combine the menu_items and order_details tables into a single table
+SELECT
+	*
+FROM
+	order_details od
+LEFT JOIN
+	menu_items mi
+ON
+	od.item_id = mi.menu_item_id;
 
 -- Task: What were the least and most ordered items? What categories were they in?
+WITH ordered_items AS(
+	SELECT
+		od.item_id, COUNT(*) AS num_of_orders, mi.category
+	FROM
+		order_details od
+	LEFT JOIN
+		menu_items mi
+	ON
+		od.item_id = mi.menu_item_id
+	GROUP BY
+		od.item_id
+)
+
+SELECT 
+	item_id, num_of_orders, category
+FROM
+	ordered_items
+ORDER BY
+	num_of_orders ASC
+LIMIT
+	1;
+    
+WITH ordered_items AS(
+	SELECT
+		od.item_id, COUNT(*) AS num_of_orders, mi.category
+	FROM
+		order_details od
+	LEFT JOIN
+		menu_items mi
+	ON
+		od.item_id = mi.menu_item_id
+	GROUP BY
+		od.item_id
+)
+
+SELECT 
+	item_id, num_of_orders, category
+FROM
+	ordered_items
+ORDER BY
+	num_of_orders DESC
+LIMIT
+	1;
 
 -- Task: What were the top 5 orders that spent the most money?
 
+SELECT
+	od.order_id, SUM(mi.price) order_total
+FROM
+	order_details od
+LEFT JOIN
+	menu_items mi
+ON
+	od.item_id = mi.menu_item_id
+GROUP BY
+	od.order_id
+ORDER BY
+	order_total DESC
+LIMIT 5;
+
 -- Task: View the details of the highest spend order. Which specific items were purchased?
+SELECT
+	*
+FROM
+	order_details od
+LEFT JOIN
+	menu_items mi
+ON 
+	od.item_id = mi.menu_item_id
+WHERE
+	od. order_id = 440;
 
 -- Task: BONUS: View the details of the top 5 highest spend orders
+
+SELECT
+	*
+FROM
+	order_details od
+LEFT JOIN
+	menu_items mi
+ON 
+	od.item_id = mi.menu_item_id
+WHERE
+	od. order_id IN (440, 2075, 1957, 330, 2675);
