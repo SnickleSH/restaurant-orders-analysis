d227e46274f2f18b70589e8c88994a8af4a614ea
Kristóf Németh <138215045+SnickleSH@users.noreply.github.com>
2025-03-09 13:36:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Restaurant Orders Analysis
+
+https://app.mavenanalytics.io/guided-projects/d7167b45-6317-49c9-b2bb-42e2a9e9c0bc
---
+++ restaurant-orders-analysis.sql
@@ -0,0 +1,33 @@
+USE restaurant_db;
+
+-- Objective 1: Explore the items table
+
+-- Task: View the menu_items table and write a query to find the number of items on the menu
+
+-- Task: What are the least and most expensive items on the menu?
+
+-- Task: How many Italian dishes are on the menu? What are the least and most expensive Italian dishes on the menu?
+
+-- Task: How many dishes are in each category? What is the average dish price within each category?
+
+-- Objective 2: Explore the orders table
+
+-- Task: View the order_details table. What is the date range of the table?
+
+-- Task: How many orders were made within this date range? How many items were ordered within this date range?
+
+-- Task: Which orders had the most number of items?
+
+-- Task: How many orders had more than 12 items?
+
+-- Objective 3: Analyze customer behavior
+
+-- Task: Combine the menu_items and order_details tables into a single table
+
+-- Task: What were the least and most ordered items? What categories were they in?
+
+-- Task: What were the top 5 orders that spent the most money?
+
+-- Task: View the details of the highest spend order. Which specific items were purchased?
+
+-- Task: BONUS: View the details of the top 5 highest spend orders
