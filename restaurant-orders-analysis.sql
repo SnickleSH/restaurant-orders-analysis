bb4a78315de6b22eb08d148dc2f5e811f8c54aed
Kristóf Németh <138215045+SnickleSH@users.noreply.github.com>
2025-03-09 15:33:56 +0100

    Objective 2 finished

--- restaurant-orders-analysis.sql
+++ restaurant-orders-analysis.sql
@@ -68,12 +68,51 @@ GROUP BY
 -- Objective 2: Explore the orders table
 
 -- Task: View the order_details table. What is the date range of the table?
+SELECT
+	*
+FROM
+	order_details;
+    
+SELECT
+	MIN(order_date), MAX(order_date)
+FROM
+	order_details;
 
 -- Task: How many orders were made within this date range? How many items were ordered within this date range?
+SELECT
+	COUNT(DISTINCT order_id)
+FROM
+	order_details;
+
+SELECT
+	COUNT(*)
+FROM
+	order_details;
 
 -- Task: Which orders had the most number of items?
+SELECT
+	order_id, COUNT(*)
+FROM
+	order_details
+GROUP BY 
+	order_id
+ORDER BY
+	COUNT(*) DESC;
 
 -- Task: How many orders had more than 12 items?
+SELECT
+	COUNT(*)
+FROM (
+	SELECT
+		order_id, COUNT(*) AS num_of_items
+	FROM
+		order_details
+	GROUP BY 
+		order_id
+	HAVING
+		COUNT(*) > 12
+) AS subquery;
+
 
 -- Objective 3: Analyze customer behavior
 
